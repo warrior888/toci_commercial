f31cf2f137ae0c9c4d5a509472594e1fe46e859f
Tomasz Lach <tomaszlach86@gmail.com>
2015-07-21 18:33:24 +0200

    dodane komentarze

--- bankApplication/scripts/bank.js
+++ bankApplication/scripts/bank.js
@@ -1,3 +1,4 @@
+//funkcja tworząca nowy obiekt bank
 function Bank(crucialNumber, getForm) {
   this.getForm       = getForm;
   this.crucialNumber = crucialNumber;
@@ -13,12 +14,14 @@ function Bank(crucialNumber, getForm) {
 // Tu będą trzymane banki.
 Bank.banks = {};
 
+//funkcja zwracająca bank o podanym numberze
 // Bank.getBank(crucialNumber: string) -> Bank
 Bank.getBank = function (crucialNumber) {
   // TODO: jakieś sprawdzenie, czy taki bank istnieje?
   return Bank.banks[crucialNumber];
 };
 
+//funkcja dodająca bank do zmiennej przechowującej banki
 // Można pomyśleć nad Bank.addBank(bank: Bank) -> undefined
 // Bank.addBank(crucialNumber: string, getForm: function) -> undefined
 Bank.addBank = function (crucialNumber, getForm) {
