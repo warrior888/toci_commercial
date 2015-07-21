eadf15241d0dd0ce67ecaa91240bab3daae49803
Tomasz Lach <tomaszlach86@gmail.com>
2015-07-21 19:37:32 +0200

    mój formularz

--- bankApplication/scripts/banks/crucial-1160.js
+++ bankApplication/scripts/banks/crucial-1160.js
@@ -1,5 +1,54 @@
 Bank.addBank('1160', function(){
 
+	var form = createElement('form',
+    { action: '#' }
+  );
+
+  var labelTargetName = createElement('label',
+    { for: 'name' },
+    { textContent: 'Adresat' }
+  );
+  var inputTargetName = createElement('input',
+    { type: 'text' },
+    { name: 'name' }
+  );
+
+  var labelTargetAmount = createElement('label',
+    { for: 'amount' },
+    { textContent: 'Kwota' }
+  );
+  var inputTargetAmount = createElement('input',
+    { type: 'text' },
+    { name: 'amount' }
+  );
+
+  var labelTargetAccount = createElement('label',
+    { for: 'account' },
+    { textContent: 'Numer konta adresata' }
+  );
+  var inputTargetAccount = createElement('input',
+    { type: 'text' },
+    { name: 'account' }
+  );
+
+  var submit = createElement('input',
+    { type:  'submit' },
+    { value: 'Zleć przelew' }
+  );
+
+
+  form.appendChild(labelTargetName);
+  form.appendChild(inputTargetName);
+
+  form.appendChild(labelTargetAmount);
+  form.appendChild(inputTargetAmount);
+
+  form.appendChild(labelTargetAccount);
+  form.appendChild(inputTargetAccount);
+
+  form.appendChild(submit);
+
+  return form;
 
 
 
@@ -11,5 +60,4 @@ Bank.addBank('1160', function(){
 
 
 
-	
 });
