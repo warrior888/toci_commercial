f5278581e52382c7c47a4ebbbb184745c714aabf
Ghost Rider <warriorr@poczta.fm>
2015-07-25 17:55:54 +0200

    zadanie

---
+++ bees/Training 2/jedendwajeden.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$liczbaDoPrzepisania = '326865643978';
+
+echo 'Dlugosc slowa: '.strlen($liczbaDoPrzepisania)."\n";
+
+for ($i = 0; $i < strlen($liczbaDoPrzepisania); $i++)
+{
+	//echo '  ----------> ' . $liczbaDoPrzepisania[$i]. ' <-----------'."\n";
+}
+
+
+
+///--------------------
+       // 01234567890
+$slowo = 'zupelnie inne';
+
+$i = strlen($slowo);
+
+
+
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
+$i--;
+echo $slowo[$i];
