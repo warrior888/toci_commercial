205ec4151fe5c86fd69271407d4eb4bb2c23c749
Tomasz Lach <tomaszlach86@gmail.com>
2015-07-25 21:38:29 +0200

    początek pracy domowej bees

---
+++ bees/Homework2/tomasz_lach/zadania.php
@@ -0,0 +1,42 @@
+<?php
+
+	// zad 0
+	
+function MojaDlugoscTextu($text)  //tozsame do strlen
+{
+	$dlugosc = 0;
+	
+	for ($dlugosc = 0; isset($text[$dlugosc]); $dlugosc++)
+	{
+		
+	}
+	
+	return $dlugosc;
+}
+
+	// zad 1
+	
+	$text = 'cokolwiek lub ciokolwiek innego';
+	
+	// o o w e  u  -> co 2 literka
+
+    // zad 2
+    
+	//dla zadania powyzej ma mi zadanialac tak, zebym mogl podac , co ktora literke ma mi wyswietlic
+	
+	// 3 k  w  k  u 
+    
+	// zad 3
+	
+	// c o o g k e o n
+	
+	// zad 4 * 
+	
+	$kwotaPrzelewu = 524 135 412;
+	// piec dwa cztery
+	
+	// piecset dwadziescia cztery miliony sto trzydziesci piec tysiecy czterysta dwanascie 
+	
+	// zad 5 ***
+	
+	// szczegolnosc osci nie prezklada sie na kosci   osc => true  dgshaj => false
