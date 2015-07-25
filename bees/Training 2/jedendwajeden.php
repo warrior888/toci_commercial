543255386041fc3cebe6df806ec78f4226fd071b
Ghost Rider <warriorr@poczta.fm>
2015-07-25 19:29:53 +0200

    zadania

--- bees/Training 2/jedendwajeden.php
+++ bees/Training 2/jedendwajeden.php
@@ -1,13 +1,47 @@
 <?php
 
 
-$liczbaDoPrzepisania = '326865643978';
+$liczbaDoPrzepisania = '4325';
 
 echo 'Dlugosc slowa: '.strlen($liczbaDoPrzepisania)."\n";
+$pelneSlowo = '';
+
+$tab = array(1 => 'jeden', 2 => 'dwa', 3 => 'trzy', 4 => 'cztery', 5 => 'piec', 8 => 'osiem');
 
 for ($i = 0; $i < strlen($liczbaDoPrzepisania); $i++)
 {
+	$pelneSlowo .= ZmienLiczbeNaText($tab, $liczbaDoPrzepisania[$i]);
+
 	//echo '  ----------> ' . $liczbaDoPrzepisania[$i]. ' <-----------'."\n";
+	
+	//$liczbaDoPrzepisania[$i]
+	
+	/*if ($liczbaDoPrzepisania[$i] == 4)
+	{
+		echo 'cztery ';
+	}
+	
+	if ($liczbaDoPrzepisania[$i] == 6)
+	{
+		echo 'szesc ';
+	}
+	
+	if ($liczbaDoPrzepisania[$i] == 8)
+	{
+		echo 'osiem ';
+	}*/
+}
+
+echo $pelneSlowo;
+                         // 0 - 9   // para do slowa
+function ZmienLiczbeNaText($tab, $liczba)
+{
+	
+	
+	//if ($liczba == $element)
+	//{
+		return $tab[$liczba]."\n";
+	//}
 }
 
 
@@ -15,22 +49,3 @@ for ($i = 0; $i < strlen($liczbaDoPrzepisania); $i++)
 ///--------------------
        // 01234567890
 $slowo = 'zupelnie inne';
-
-$i = strlen($slowo);
-
-
-
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
-$i--;
-echo $slowo[$i];
