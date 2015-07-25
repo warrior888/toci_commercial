23247c5e89548712601b11fa1efa14aad541eebb
Szpula <t.suska95@gmail.com>
2015-07-25 15:40:06 +0200

    zadanko

---
+++ bees/Training 2/szpula2.php
@@ -0,0 +1,31 @@
+<?php
+
+		$wartosc = 10;
+		
+		if($wartosc<1 and $wartosc>30)
+		{
+			echo "Wartosc jest zla";
+		}
+		if($wartosc>=1 and $wartosc<=7)
+		{
+			echo "Pierwszy tydzien";
+		}
+		if($wartosc>=8 and $wartosc<=14)
+		{
+			echo "Drugi tydzien";
+		}
+		if($wartosc>=15 and $wartosc<=21)
+		{
+			echo "Trzeci tydzien";
+		}
+		if($wartosc>=22 and $wartosc<=28)
+		{
+			echo "Czwart tydzien";
+		}
+		if($wartosc>=29 and $wartosc<=30)
+		{
+			echo "Piaty tydzien";
+		}
+		
+		
+?>
