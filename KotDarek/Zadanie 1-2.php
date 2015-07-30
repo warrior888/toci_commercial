ab60c59d18f3fb21788dfed72e30188fb895279f
Kot/Darek <dariuszkusz@o2.pl>
2015-07-30 20:04:50 +0200

    Rozwiazanie zadan 1-3

---
+++ KotDarek/Zadanie 1-2.php
@@ -0,0 +1,17 @@
+<?php
+/*
+$text = 'cokolwiek lub cokolwiek innego';
+
+for ($i= 1; $i<strlen($text);$i+=2)
+{
+	echo $text[$i];
+}
+*/
+
+
+/*
+for ($i= 2; $i<strlen($text);$i+=3)
+{
+	echo $text[$i];
+}
+*/
