d329d8902ed3294abd16b7eee9056aa3f5102a47
Tomasz Lach <tomaszlach86@gmail.com>
2015-08-08 07:49:01 +0200

    zad 3

--- bees/Homework3/TomaszLach/praca_domowa_3.php
+++ bees/Homework3/TomaszLach/praca_domowa_3.php
@@ -14,18 +14,19 @@ function insertSpace($accNumber, $place=4)
 		}
 	}
 
-	return $output."\n";
+	return $output;
 }
 
-echo insertSpace("012345678901234567890123");
-echo insertSpace("012345678901234567890123", 6);
-echo insertSpace("012345678901234567890123", 5);
+echo insertSpace("012345678901234567890123")."\n";
+echo insertSpace("012345678901234567890123", 6)."\n";
+echo insertSpace("012345678901234567890123", 5)."\n";
 
 //zad2
 //$jump - zmienna określająca który element będzie zamieniony z zerowym
 function swapPlace($accNumber, $jump = 2)
 {
 	$output = $accNumber;
+	$temp = "";
 	for($i=0; $i<strlen($output);$i++)
 	{
 		if($i % $jump == 0)
@@ -35,7 +36,31 @@ function swapPlace($accNumber, $jump = 2)
 			$output[$i+($jump-1)] = $temp;
 		}
 	}
-	return $output."\n";
+	return $output;
 }
 
-echo swapPlace("012345678901234567890123");
+echo swapPlace("012345678901234567890123")."\n";
+
+//zad2a
+function insertAndSwap($accNumber)
+{
+	return insertSpace(swapPlace($accNumber));
+}
+
+echo insertAndSwap("abcdefghijklmnopqrstuvwxyz")."\n";
+
+//zad 3
+$pattern = array(3 => 1, 2=>5, 1=>6);
+var_dump($pattern);
+
+function patternSwap(array $pattern, $accNumber="abcdefghijklmnopqrstuvwxyz")
+{
+	$output = "00000000000000000000000000";
+	foreach($pattern as $key => $value)
+	{
+		$output[$value] = $accNumber[$key];
+	}
+	return $output;
+}
+
+echo patternSwap($pattern)."\n";
