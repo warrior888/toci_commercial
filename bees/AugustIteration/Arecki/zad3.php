39ca83b512a3691e78765495798898af618462df
Arecki <arek.jelonek@gmail.com>
2015-08-21 21:01:00 +0200

    no message

---
+++ bees/AugustIteration/Arecki/zad3.php
@@ -0,0 +1,16 @@
+<?php
+
+    $tekst1 = 'Arek';
+    $tekst2 = 'Arecki';
+    $x = strlen($tekst1)-1;
+    $y = strlen($tekst2)-1;
+
+    if ($x == $y) {
+    
+    for ($i = 0; $i <= $x; $i++) {
+       if (tekst1 == tekst2) {
+       		echo 'Teksty takie same';
+       } else echo 'Teksty rozne';
+           } 
+    } else echo 'Teksty rozne';
+?>
