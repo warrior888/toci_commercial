c069284831cab9b741d1d8e9f9dcbaec7bf3afe9
Sebastian Michałowski <seb.michalowski@gmail.com>
2015-08-22 19:20:53 +0200

    Rozpoznaj kod Banku ver. 2

---
+++ bees/AugustIteration/Ubot (Seba)/rozpoznaj_kod_banku_seb.php
@@ -0,0 +1,61 @@
+<?php
+//Zmienne
+	$numerKonta = "12" .$bankCode ."321618181";
+
+	$bankCode = "1030";
+	$bankID = array("1020" => "ING", "1030" => "PKO"); // 1050
+	$error = "bledny kod banku";
+//funkcja
+	function ZwrocBank() {
+
+		if ($bankCode == $bankID[1020])
+		{
+			echo = $bankID[1020];
+		}
+		elseif ($bankCode == $bankID[1030]) {
+			echo = $bankID[1030];
+		}
+		else {
+			echo = $error;
+		}
+	}
+
+//Funkcja lepiej
+
+function ZwrocBankLepiej() {
+
+	if ($bankCode == $bankID[$bankCode])
+	{
+		echo = $bankID[$bankCode];
+	}
+	elseif ($bankCode == $bankID[$bankCode]) {
+		echo = $bankID[$bankCode];
+	}
+	else {
+		echo = $error;
+	}
+}
+
+//Funkcja Najlepiej
+
+	function ZwrocBankNajlepiej($kod) {
+
+		if ($bankCode == $bankID[$kod])
+		{
+			echo = $bankID[$kod];
+		}
+		elseif ($bankCode == $bankID[$kod]) {
+			echo = $bankID[$kod];
+		}
+		else {
+			echo = $error;
+		}
+	}
+
+	echo ZwrocBank();
+
+	echo ZwrocBankLepiej();
+
+	echo ZwrocBankNajlepiej($kod)
+
+?>
