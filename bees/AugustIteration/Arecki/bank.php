7ae343a1939c4afffb253b83ca86339b28ffb40f
Arecki <arek.jelonek@gmail.com>
2015-08-22 19:30:45 +0200

    DLaczego echo nie wyswietla nazwy banku ? ;/

--- bees/AugustIteration/Arecki/bank.php
+++ bees/AugustIteration/Arecki/bank.php
@@ -4,7 +4,24 @@
     $bankCode = substr($numerKonta, 2, 4);
     $bankID = array("1020" => "ING", "1030" => "PKO");
     
-   if ($bankCode == 1020) {
+   echo $bankCode;
+//funkcja
+	function ZwrocBank($kod) {
+
+		if (isset($bankID[$kod])) // 1050 => false
+		{
+			return $bankID[$kod];
+            echo $bankID[$kod];
+		}
+		else {
+			return null;
+		}
+
+	}
+
+	echo ZwrocBank($bankCode);
+/*
+if ($bankCode == 1020) {
         echo 'ING';
    } else if ( $bankCode == 1030) {
         echo 'PKO';
@@ -15,6 +32,4 @@
    } else if ( $bankCode == 1060) {
         echo 'Spoldzielczy';
    }
-
-    
-    
+*/
