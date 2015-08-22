4d5ae05e634abb2255f14c4459dc2cb3c7c65cef
Slawomir Herman <slawomirherman@icloud.com>
2015-08-22 12:50:42 +0100

    cwiczenia

---
+++ bees/AugustIteration/Slawekkk/test.php
@@ -0,0 +1,21 @@
+<?php
+
+	$ocena = 1;
+
+	if ($ocena > 6 || $ocena < 1) //warunek do niepoprawnej oceny; >6 ORAZ oceana >1
+	{
+		echo 'Ocena niepoprawna';
+	}
+	else
+	{
+		echo 'Ocena poprawna';
+		
+		if (ocena == 1)
+		{
+			echo 'Jedyneczka';
+		}
+		
+		
+		
+		
+	}
