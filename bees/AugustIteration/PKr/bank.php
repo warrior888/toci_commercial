1bf60fe52f66076b360ed022d7bbe7bb41e78722
pawelk30 <pawelkrysztopowicz@gmail.com>
2015-08-23 18:36:25 +0200

    taki plagiat delikatny :P

---
+++ bees/AugustIteration/PKr/bank.php
@@ -0,0 +1,19 @@
+<?php
+
+$kontobankowe = "10 2040 7853 3939 2929";
+
+$wycinek = 1040;
+
+if ($wycinek == 2040)
+{
+	echo 'bank bzwbk';
+}
+if ($wycinek == 1040)
+{
+	echo 'bank pko';
+}
+if ($wycinek == 2050)
+{
+	echo 'bank ing';
+}	
+
