ad131dda069adc755560dfc3da7a04696b12929b
Sebastian Michałowski <seb.michalowski@gmail.com>
2015-08-23 17:47:44 +0200

    Bank_od_do v1

---
+++ bees/AugustIteration/Ubot (Seba)/Bank_od_do.php
@@ -0,0 +1,50 @@
+<?php
+
+//Zmienne
+$od = "od";
+$do = "do";
+
+$listaPrzelewow = array
+(
+array($do =>'121030543212345', $do =>'121050543212345'),
+array($do =>'121050543212345', $do =>'121020543212345'),
+array($do =>'121050543212345', $do =>'121030543212345'),
+array($do =>'121030543212345', $do =>'121030543212345'),
+array($do =>'121020543212345', $do =>'121050543212345'),
+);
+
+$kodBanku = array(1030 =>"ING", 1050 =>"PKO", 1020 => "Milenium");
+
+
+//Funkcje
+function znadzNrKonta ($przelewy){
+	for ($i = 0; i < count($przelewy); $i++) {
+
+		$pojedynczyPrzelew = $przelewy[$i];
+
+		$wyciagnijKodOd = substr ($pojedynczyPrzelew[$od], 2, 4);
+		$wyciagnijKodDo = substr ($pojedynczyPrzelew[$do], 2, 4);
+		$przelewOd = $wyciagnijKodOd[$kodBanku];
+		$przelewDo = $wyciagnijKodDo[$kodBanku];
+
+		echo "Przelew od" . $przelewOd . " , do " .$przelewDo;
+
+		}
+
+//function zwrocBankiOdDo ($kod) {
+//	$przelewWychodzacy = $
+//
+// 	if (wyciagnijKodOd[$kod] && wyciagnijKodDo == kodBanku[$kod])
+// 	{
+// 	 echo "Przelew z " .
+ //	}
+//}
+
+function pobierzKodBanku ($nrKonta){
+	return substr ($nrKonta, 2, 4);
+};
+//wykonaj kod
+
+znajdzNrKonta($listaPrzelewow);
+
+?>
