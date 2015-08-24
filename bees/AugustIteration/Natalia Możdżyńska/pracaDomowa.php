d5ce73e94b00570e00ecbb5c63c4935071f4724c
Natalia Możdżyńska <natalia.mozdzynska@o2.pl>
2015-08-24 22:24:22 +0200

    commit

---
+++ bees/AugustIteration/Natalia Możdżyńska/pracaDomowa.php
@@ -0,0 +1,14 @@
+<?php
+ echo "kot" ;
+ echo " marianna \n";
+ $dzie=30 ;
+ $dowolna = 200 ;
+ $dzie = $dowolna - 2 ;
+ echo "$dzie \n" ;
+ echo "nowa lekcja \n \n \n";
+ 
+ $zmienna=10 ;
+ if ($zmienna>5)
+ { echo 'zmienna wieksza od 5';
+ }
+?>
