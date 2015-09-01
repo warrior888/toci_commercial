77a0b22280f9b9b9337cd3df29ddf5efd7cfccd5
amarek <amarek@L00358.ic>
2015-09-01 12:37:33 +0200

    zadania

---
+++ bees/AugustIteration/Training2/Artur/zad2.php
@@ -0,0 +1,25 @@
+<?php
+
+	function Palindromy($text)
+	{
+		$slownik = array ('zaraz', 'kajak', 'anna', 'sedes', 'mam', 'ala', 'radar',
+						  'sus', 'zakaz', 'sos', 'pop', 'aga', 'oko', 'kok', 'abba', 
+						  'inni', 'oto', 'aha', 'olo', 'ono');
+		
+		$dl = count ($slownik);
+
+		for ($i = 0; $text < $dl; $i++)
+		{
+			if ($text === $slownik[$i])
+			{
+				echo "\n".'prawda';
+				break;
+			}
+			else
+			{
+				echo "\n".'klamstwo';
+			}
+		}
+	}
+	
+	echo Palindromy('ono');
