2bc024be061b34026fd9f6cee85a316e2859a94e
Sebastian Michałowski <seb.michalowski@gmail.com>
2015-09-02 21:09:38 +0200

    .

--- bees/AugustIteration/Ubot (Seba)/zadania tydzien 2/zad4_drugie_podejscie.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Zad4 - Zamień w tekście pewien ciąg znaków na inny, a reszte zostaw na swoim miejscu. "(mama, ma, ta) ---> wynik= "tata"
-
-function SwichString ($string, $swichFrom, $swichTo){
-
-}
-
-SwichString ("mama", "ma", "ta")
