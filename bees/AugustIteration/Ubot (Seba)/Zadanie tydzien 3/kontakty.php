64f74e3e64b3e3efb8e35bbea8b1fe3bb0b6808d
Sebastian Michałowski <seb.michalowski@gmail.com>
2015-09-05 10:12:42 +0200

    tablica kontakty

--- bees/AugustIteration/Ubot (Seba)/Zadanie tydzien 3/kontakty.php
+++ bees/AugustIteration/Ubot (Seba)/Zadanie tydzien 3/kontakty.php
@@ -1,16 +1,17 @@
 <?php
 
-$kontakty = array('Grupa_Rodzina' => array(
-                    'Mama' => "+48 900 900 900", 'Brat' => "+48 777 900 977", 'Kuzyn' => "+48 900 900 900" )
-                'Grupa_Znajomi' => array(
-                    'Patryk' => "+48 111 111 111", 'Rafal' => "+48 222 333 666", 'Ola' => "+48 999 111 444" )
-                'Grupa_Praca' => array(
-                    'Ksiegowa' => "+48 510 510 910", 'CEO' => "+48 777 900 977", 'Sekretarka' => "+48 300 933 900" ));
+$kontakty = ['grupy' =>
+                ['Grupa_Rodzina' =>
+                    ['Mama' => '+48 900 900 900', 'Brat' => '+48 777 900 977', 'Kuzyn' => '+48 900 900 900'],
+                'Grupa_Znajomi' =>
+                    ['Patryk' => '+48 111 111 111', 'Rafal' => '+48 222 333 666', 'Ola' => '+48 999 111 444'],
+                'Grupa_Praca' =>
+                    ['Ksiegowa' => '+48 510 510 910', 'CEO' => '+48 777 900 977', 'Sekretarka' => '+48 300 933 900' ]]];
 var_dump($kontakty);
 
 function ZnajdzKontakt($array,$nazwa)
 {
-    foreach ($array as $key => $value) {
+    foreach ($array as $value) {
         if (is_array($key)) {
 
             $przypisanie = function ZnajdzKontakt($array,$nazwa);
