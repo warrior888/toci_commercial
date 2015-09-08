ee75f4f65b18359a87d9e135a6a599f1f61da467
Arecki <arek.jelonek@gmail.com>
2015-09-08 19:35:17 +0200

    Wypisywanie danych  z tablicy uzupelnione o foreacha

--- bees/AugustIteration/Arecki/Week 3/wypisywanie_danych_z_tablicy.php
+++ bees/AugustIteration/Arecki/Week 3/wypisywanie_danych_z_tablicy.php
@@ -16,5 +16,18 @@
 
 echo 'Numer Luka to: ' . $example['Luk']['telefon'];
 
+$AdrianK = $example['AdrianK']['Dzieci'];
+
+foreach ($AdrianK as $child) {
+
+    echo 'Dzieci Adriana  sa ' . $child . "\n";
+}
+
+$UbotCars = $example['Ubot']['samochody'];
+
+foreach ($UbotCars as $car) {
+
+    echo ' ' . $car . "\n";
+}
 
 ?>
