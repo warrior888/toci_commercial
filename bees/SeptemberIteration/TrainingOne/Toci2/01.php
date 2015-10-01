0e20cd7fe080d4ecdb8cdc664bebc22addb92aac
Lukkkk <nosferatu7>
2015-10-01 22:08:34 +0200

    Moje zadania domowe

---
+++ bees/SeptemberIteration/TrainingOne/Toci2/01.php
@@ -0,0 +1,14 @@
+<?php 
+
+//Z przedziału liczb 1 do 100 wyświetl wszystkie parzyste.
+//Dodaj do siebie i zwróć wszystkie liczby z przedziału 20-30.
+
+    for ($i=0; $i <= 100; $i++)
+	{
+        
+            if ($i % 2 === 0)
+				{
+                echo $i; 
+				}  	
+    }
+
---
+++ bees/SeptemberIteration/TrainingOne/Toci2/02.php
@@ -0,0 +1,15 @@
+<?php
+
+//Dodaj do siebie i zwróć wszystkie liczby z przedziału 20-30.
+
+
+for ($i=0; $i <100; $i++)
+	{
+            if ($i>19 && $i<31)
+				{
+                $suma = $suma +$i; 
+				}  
+	
+    }
+
+	echo $suma; 
