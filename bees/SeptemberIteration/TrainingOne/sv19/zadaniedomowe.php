5f6dbd631290eb3caf8773aa3a4609427a158af6
Robert Śmietanowski <sv19@wp.pl>
2015-10-02 21:14:53 +0200

    praca domowa w wersji spaghetti ;)

---
+++ bees/SeptemberIteration/TrainingOne/sv19/zadaniedomowe.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+for ($i = 1; $i <= 100; $i++) 
+{
+    if ($i % 2 === 0) 
+    {
+        echo"$i \n";
+    }
+}
+
+
+for($i=1; $i<=100; $i++)
+
+$sum+=$i;
+
+        echo "Suma liczb wynosi $sum";
+echo "\n";
+
+for($i=1; $i<=100; $i++)
+{
+   if ($i > 19 && $i <31) 
+   {     
+       echo "$i \n";
+}        
+}
+
