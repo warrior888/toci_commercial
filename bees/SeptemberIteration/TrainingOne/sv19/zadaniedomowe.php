7d270f181b02d8758705477febcffc4074b8a606
Robert Śmietanowski <sv19@wp.pl>
2015-10-03 14:18:53 +0200

    no message

--- bees/SeptemberIteration/TrainingOne/sv19/zadaniedomowe.php
+++ bees/SeptemberIteration/TrainingOne/sv19/zadaniedomowe.php
@@ -1,26 +1,26 @@
  <?php
-
-for ($i = 1; $i <= 100; $i++) 
+$sum =0;
+for ($i=1; $i<=100; $i++) 
 {
     if ($i % 2 === 0) 
     {
        echo"$i \n";
     }
 }
-
-
-for($i=1; $i<=100; $i++)
+for ($i = 1; $i <= 100; $i++) 
+{
     if ($i % 2 === 0) 
-    $sum+=$i;
     {
-       echo "Suma liczb wynosi $sum";
-       echo "\n";
+        $sum+=$i;
     }
+}
+
+echo "Suma liczb wynosi $sum \n";
+
 for($i=1; $i<=100; $i++)
 {
    if ($i > 19 && $i <31 )//&& $i % 2 === 0) 
-    {     
-       echo "$i \n";
+    {   
+      echo "$i \n";
     }        
 }
-
