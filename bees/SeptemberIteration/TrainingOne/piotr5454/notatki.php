5742933c98c06b205e0f77312b389a89ffb9df98
Piotr Młyńczak <drago5454@wp.pl>
2015-10-09 09:09:27 +0200

    rozwiązanie zadania wieczornego z 26.09 i notatki

--- bees/SeptemberIteration/TrainingOne/piotr5454/notatki.php
+++ bees/SeptemberIteration/TrainingOne/piotr5454/notatki.php
@@ -4,7 +4,12 @@ $how = "wesolo"; /*każda zmienna ma swój typ, ta jest typu string czyli
  * przechowuje jakiś ciąg znaków
  */
 
+$age1 = 15;
 $age = 15; //zmienna typu int
+
+echo "piotr5454"."\n"."dzialam ". //\n robi enter
+    $how." od ".
+    $age." lat."; 
 
 $myProfile = array($how, $age, 'Ibiza');/*zmienna typu tablicowego, może 
  * przechowywać wewnątrz () jakieś inne zmienne, ciąg znaków
@@ -24,10 +29,43 @@ var_dump($boolTrue);
 
 var_dump($nullVar);
 
-echo "piotr5454"."\n"."dzialam ". //\n robi enter
-    $how." od ".
-    $age." lat."; 
+//operatory arytmetyczne
+echo $age1 + $age . "\n";
+echo $age1 - $age . "\n";
+echo $age1 * $age . "\n";
+echo $age1 / $age . "\n";
+echo $age1 % $age . "\n"; //modulo
+
+//operatory przypisania i przyrównania =,==,===
+//instrukcje warunkowe sprawdzają czy jakiś warunek jest spełniony
+
+$myAge = 43;
+$fatherAge = 57;
+
+if ($myAge == $fatherAge) //wyświetli false, bo wartości nie są równe
+{
+    echo "true";
+}
+
+else {
+    
+echo "false"; }
 
 
+if ($myAge === $fatherAge) //wyświetli false, bo i wartości i typy nie są równe
+{
+    echo "true";
+}
+
+else {
+    
+echo "false"; }
 
 
+$zmienna1 = 30;
+
+if($zmienna1 % 2 === "0") // 30 jest podzielne przez inna liczbę w tym przypadku
+                          //przez 2 bo reszta z dzielenia wynosi 0
+{
+
+}
