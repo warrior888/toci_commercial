f72d9a70d01047d11b9a92ba315a047969a75609
Piotr Młyńczak <drago5454@wp.pl>
2015-10-28 19:07:14 +0100

    arrays

--- bees/SeptemberIteration/TrainingSix/piotr5454/DataTypes.php
+++ bees/SeptemberIteration/TrainingSix/piotr5454/DataTypes.php
@@ -1,13 +1,121 @@
 <?php
 
-//RZUTOWANIE TYPÓW, zrobić z INT typ STRING
+// ------ ZADANIE 1 ------
+//napiszemy ile razy mamy taką sytuację że cyfra następna jest o 1 większa od poprzedniej
 
-$number = 123345615;
+/*pętlą mozna przechodzić tak jak my byśmy tego teraz potrzebowali po ciągach znaków albo po tablicach
+ * więc musimy zmienić typ zmiennej $number z int na string czyli zrzutować zmienną
+ */
 
-//<?php
-//$number = 12385342534; //cyfry 0-9 3 6 14 19 22
-//$string = (string)$number;
-//$length = strlen($string);
+$number = 1238534253; //cyfry 0-9
+$string = (string)$number;/* do zmiennej $string przypisz zrzutowanie wartości $number na typ stringowy
+                           * i od teraz możemy przechodzić pętlą np. for po tej liczbie                  
+                           */
+
+//możemy teraz wyświetlić tylko 23 z całej $number 
+
+//echo $string[1].$string[2];/* wyświetl pozycje 1 i 2 ze $stirng; jak w układach mikroprocesorowych pozycje 
+//                            * są liczone od zera więc nad cyfrą 1 jest zero, nad cyfrą 2 jest jeden itd.  
+//                            */
+//
+$length = strlen($string); /* żeby wiedzieć ile razy mamy przejść pętlą potrzebujemy informacji o długości ciągu znaków,
+                            * używamy funkcji strlen która liczy ilość znaków danego stringa przypisując ją do $length
+                            * $length przechowuje teraz ilość ciągu znaków $number,
+                            * dzieki temu możemy użyć teraz wartości $length w pętli
+                            */
+//
+//for ($i = 0; $i < $length; $i++) {
+//    echo $string [$i]. "\n"; }// możemy teraz używając pętli wyświetlić np. każdą cyfrę z osobna
+//
+//
+//// ...cyfra następna jest o 1 większa od poprzedniej
+//    
+//    
+//for ($i = 0; $i < $length; $i++) 
+//{   $counter = 0
+//    
+//        if($string[$i] = $string[$i - 1] + 1) /* jeżeli dana $string dla elementu $i jest równa $string od $i - 1,
+//                                              * za [$i-1] trzeba dodać 1 bo szukamy cyfry o jeden większej od poprzedniej
+//                                              */                          
+//    {
+//        $counter++;                          /* to podbij licznik ($counter) ustawiony na indeks zero
+//                                              * CZYLI LICZNIK ZACZYNA OD CYFRY Z INDEKSEM ZERO CZYLI OD 1
+//                                              * i PODBIJA CZYLI PRZECHODZI PO PROSTU Z CYFRY 1 NA KOLEJNĄ CYFRĘ itd.
+//                                              */
+//    }
+//}
+
+/*w kodzie powyższym są dwa błędy:
+ * 1. $i nie może równać się 0 bo skoro [$i - 1] to od zera odejmie jeden i będzie chiała podstawić indeks -1 który
+ * nie istnieje, więc wystarczy zmienić wartość początkową $i z 0 na 1
+ * 2. ta pętla przechodzi 10 razy i za każdym razem do $counter przypisuje 0, ewentualnie wyświetli na końcu 
+ * $counter równy 1 lub 0, bo albo trafi na taki przypadek na końcu pętli albo nie, czyli $counter powinien być wyrzucony
+ * na zewnątrz 
+ *              !!!!!!!!!!! NIGDY NIE URZYWAĆ LICZNIKÓW WEWNĄTRZ PĘTLI !!!!!!!!!!!!!
+ *
+ * WIĘC KOD POWINIEN WYGLĄDAĆ TAK JAK PONIŻEJ 
+ */
+//$counter = 0;
+//         
+//for ($i = 1; $i < $length; $i++) 
+//{  
+//    
+//        if($string[$i] === $string[$i - 1] + 1) 
+//    {
+//        $counter++;                          
+//    }
+//} 
+
+//$counter = 0;
+//         
+//for ($i = 1; $i < $length; $i++) 
+//{  
+//    
+//        if((int)$string[$i] === (int)$string[$i - 1] + 1)/* bez zrzutowania program by się wykonał bo porównujemy stringi
+//                                                          * ale w przyszłości gdybyśmy chcieli zrobić coś z tymi liczbami
+//                                                          * to bezpieczniej jest rzutować je wczesniej  
+//                                                          */ 
+//    {
+//        $counter++;                          
+//    }
+//} 
+
+//  ------ ZADANIE 2 ------
+// dodaj do siebie wszystkie cyfry ze $number i wyświetl wynik
+/* trzeba będzie użyć operatora przypisania z operatorem wykonującym pewne działanie na argumentach czyli
+*  
+ * += - przypisuje sumę bierzącej wartości zmiennej docelowej i argumentu 
+ * 
+ * działa to na takiej zasadzie, że bieżąca wartość docelowa to jest kolejna cyfra z ciągu znaków plus argument [$i]
+ * czyli najpierw 1 będzie bierzącą wartością potem program postawi znak + i przypisze tej sumie argument czyli kolejną
+ * cyfrę ze stronga czyli 2, potem dwa będzie wartością docelową znowu postawi + i doda kolejny argument czyli 3 itd
+ */
+//$counter = 0;
+
+//for ($i = 0; $i < $length; $i++)
+//{
+//    $counter += (int)$string[$i]; /* $counter podbijamy o aktualną wartość liczbową zmiennej $string
+//                                   * CZYLI LICZNIK PRZECHODZI NA KOLJENĄ WARTOŚĆ ZE STRINGA I DODAJE JĄ DO POPRZEDNIEJ
+//                                   */
+//}
+
+//  ------ ZADANIE 3 ------
+//  napisz funkcję która sprawdzi czy dana liczba ma n cyfr
+//  
+
+function example($number, $n)
+{
+     $string = (string)$number;
+     $length = strlen($string);
+     if ($length === $n)
+     {
+         return true;
+     }
+     return false;
+}
+
+var_dump(example(122324, 10));
+
 //$counter = 0;
 //for ($i = 0; $i < $length; $i++) {
 //    // $counter += (int)$string[$i];
--- bees/SeptemberIteration/TrainingSix/piotr5454/piotr5454_dataTypes.php
+++ bees/SeptemberIteration/TrainingSix/piotr5454/piotr5454_dataTypes.php
@@ -1,45 +1,28 @@
 <?php
 //Sprawdź, czy dany ciąg liczbowy ma parzystą ilość cyfr
-//
-function Parzysta($number)
-{
 
-	$string = (string)$number;
-	$lenght = strlen($string);
-	
-	$counter=0;
-	for($i=0; $i<$lenght; $i++)
-        {
-	$counter = $counter+1;
-	
-	if ($counter===$number)
-		{
-		return true;
-		}
-	}
-return false;	
-}       
-Parzysta(1234);
 
-////Sprawdź, czy dany ciąg liczbowy ma ilość cyfr podzielną przez n
-//	
-function SprawdzPodzielnosc($number,$n)
+
+//Sprawdź, czy dany ciąg liczbowy ma ilość cyfr podzielną przez n
+	
+function iloscCyfrPodzielna($number,$n)
 {	
 	$string = (string)$number;
 	$lenght = strlen($string);
 	
 	$counter=0;
-	for($i=1; $i<=$lenght; $i++)
-        {
-            if ($i%$n===0)
+	for($i=0; $i<=$lenght; $i++)
+        { 
+            $counter++;
+                if ($counter%$n===0)
 		{
-		$counter = $counter+1;
+                    echo $counter."\n";
 		}
         }  
-    return $counter;
+    
 }
 	
-echo SprawdzPodzielnosc(123456789447,3);
+iloscCyfrPodzielna(234567894,3);
 
 
 //Stwórz drzewo genealogiczne - tablica $rodzina, dziadkowie, rodzice, dzieci
