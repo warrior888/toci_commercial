9255bcc6dd40e842db02691c2ba71bb4ce8fc4ae
sparrow41 <drapikpatryk@gmail.com>
2016-04-05 21:42:03 +0200

    if

--- bees/April2016Iteration/training 1/pd_tr1/pd_tr1.php
+++ bees/April2016Iteration/training 1/pd_tr1/pd_tr1.php
@@ -3,17 +3,22 @@
 	$wiek = 19;
 
 
-	if ($wiek >15 && $wiek >21)
+	if ($wiek >15 && $wiek < 21)
 	{
-		echo 'Szko³a œrednia';
+		echo 'szkola srednia';
 	}
 	
-	else if ($wiek > 21 && $wiek > 25)
+	else if ($wiek > 21 && $wiek < 25)
 	{
 		echo 'Studia';
 	}
 	
-	else if($wiek > 25)
+	else if($wiek > 25 && $wiek < 65)
 	{
 		echo 'Praca';
+	}
+	
+	else if($wiek > 65)
+	{
+		echo 'emerytura :D';
 	}
