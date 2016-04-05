c8e4f2559ddf69dd9807da95c3064c984d1271d4
SSJ <programista_88@wp.pl>
2016-04-05 20:43:19 +0200

    Zadanie :)

--- bees/April2016Iteration/training 1/ss_tr1/ss_tr1.php
+++ bees/April2016Iteration/training 1/ss_tr1/ss_tr1.php
@@ -1,3 +1,43 @@
 <?php
 
-echo "To dzia³a"
+	echo "\n";
+	echo 'To dzia³a';
+	echo "\n";
+
+	//zmienne
+	$wiek = 12;
+	$nazwa = 'tresc';
+	$limitWiekuDoroslosci = 18;
+	
+	//operatory
+	// + - * / || && == === < >= => =<
+	//true false null
+	
+	//instrukcja warunkowa
+	if ($wiek >= $limitWiekuDoroslosci)
+	{
+		echo 'Osoba jest dorosla';
+	}
+	else
+	{
+		echo 'Mamy tu dziecko';
+	}
+	
+	if ($wiek > 6 && $wiek < 12)
+	{
+		echo 'podstawowka';
+	}
+	
+	if ($wiek >= 12 && $wiek < 16)
+	{
+		echo 'gimanzjum';
+	}
+	
+	if ($wiek <= 16 && $wiek < 19)
+	{
+		echo 'liceum';
+	}
+	
+	echo "\n";
+	
+	//petla
