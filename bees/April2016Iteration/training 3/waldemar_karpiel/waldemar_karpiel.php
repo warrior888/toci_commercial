8d4ff4f924bb17819c65729628bc54f4fa9a7471
Waldemar <karpiel.waldemar@gmail.com>
2016-04-06 22:24:34 +0200

    tr_3 strpos

--- bees/April2016Iteration/training 3/waldemar_karpiel/waldemar_karpiel.php
+++ bees/April2016Iteration/training 3/waldemar_karpiel/waldemar_karpiel.php
@@ -17,15 +17,27 @@ function wk_palindrome($text)
     return $text == wk_strrev($text);
 }
 
-//function wk_strpos($myString, $findMe)
-//{
-//    for ($i = 0; $i <= strlen($myString); $i++)
-//    {
-//        if ($myString == $findMe)
-//        {
-//
-//        }
-//    }
-//}
+function wk_strpos($myString, $findMe)
+{
+    if (strlen($myString) >= strlen($findMe)) {
+        for ($i = 0; $i <= (strlen($myString) - 1); $i++)
+        {
+            if ($myString[$i] == $findMe[0])
+            {
+                for ($x = 0; $x <= (strlen($findMe) - 1); $x++)
+                {
+                    if(isset($myString[($i + $x)])) {
+                        if($myString[($i + $x)] == $findMe[$x])
+                        {
+                            if($x == (strlen($findMe) - 1)) {
+                                return $i;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-echo wk_strpos('waldemar', 'ald');
+echo wk_strpos('waldemar', 'emar');
