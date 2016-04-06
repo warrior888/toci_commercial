fd46a2f7cac256969063df71f4074160fc3aeeac
Klaudia Wiszowata <klaudiawiszowata24@gmail.com>
2016-04-06 19:40:12 +0200

    ify

--- bees/April2016Iteration/training 1/kw_tr1/kw_tr1.php
+++ bees/April2016Iteration/training 1/kw_tr1/kw_tr1.php
@@ -1,1 +1,62 @@
 <?php
+	echo "\n";
+	echo 'To dziala';
+	echo "\n";
+	
+	
+	//zmienne
+	$wiek = 12;
+	$nazwa = 'tresc';
+	
+	$zinterpretowany = 'Moj wiek to: ' . $wiek;
+	
+	$limitWiekuDoroslosci = 18;
+	
+	//operatory
+	// + - * / || && == === < > >= <= .
+	
+	//instrukcja warunkowa
+	if ($wiek > $limitWiekuDoroslosci)
+	{
+		echo 'Osoba jest dorosla';
+	}
+	else
+	{
+		echo 'Dzieciak';
+	}
+	
+	echo "\n";
+	
+	if( $wiek >6 && $wiek <12)
+	{
+		echo 'podstawowka';
+	}
+	
+	if( $wiek >=12 && $wiek <15)
+	{
+		echo 'gimbaza';
+	}
+	
+	if( $wiek >=15 && $wiek <20)
+	{
+		echo 'technikum';
+	}
+	
+	if( $wiek >=20 && $wiek <25)
+	{
+		echo 'studia';
+	}
+	
+	if( $wiek >=25 && $wiek <67)
+	{
+		echo 'praca zawodowa';
+	}
+	
+	if( $wiek >=67 && $wiek <999)
+	{
+		echo 'emerytura';
+	}
+	
+	echo "\n";
+	
+	//petla
