847ad9301de16065452126c916d64d7edf68b402
Radosław Makowski <makowski.rado@gmail.com>
2016-04-06 21:49:59 +0200

    no message

--- bees/April2016Iteration/training 3/radoslaw_makowski/radoslaw_makowski.php
+++ bees/April2016Iteration/training 3/radoslaw_makowski/radoslaw_makowski.php
@@ -1,11 +1,12 @@
 <?php
 	/*===================================== Szkolenie 3 =====================================*/
 	$trescTextowa = 'cokolwiek';
-	echo $trescTextowa[1] . $trescTextowa[7];
+	echo "\n" . $trescTextowa[1] . $trescTextowa[7];
 	echo "\n";
 	echo 'Dlugosc tekstu: ' . strlen($trescTextowa) . "\n";
 	echo "Moja dlugosc tekstu: " . rm_strlen($trescTextowa);
 	echo "\n";
+	echo rm_strrev($trescTextowa) . "\n";
 	function rm_strlen($text)
 	{
 		for ($i = 0; isset($text[$i]); $i++)
@@ -13,11 +14,21 @@
 		}
 		return $i;
 	}
-	function in_strrev($text)
+	function rm_strrev($text)
 	{
-		for ($i = 0; ;)
+		$dlugosc = rm_strlen($text);
+		$newtxt = '';
+		for ($i = $dlugosc-1; $i >= 0; $i--)
 		{
-			
+			$newtxt .= $text[$i];
+			return $newtxt;
 		}
 	}
-?>
+	
+	//w domu funkcje do napisania:
+	//strpos
+	//strrev
+	//substr
+	//palindrom *
+	//strtr *
+	//str_replace
