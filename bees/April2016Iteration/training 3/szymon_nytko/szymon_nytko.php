574a174cf54d2879aa1a2aba785caa510e8ce874
Szymon <rajdan3@interia.pl>
2016-04-06 22:33:53 +0200

    no message

--- bees/April2016Iteration/training 3/szymon_nytko/szymon_nytko.php
+++ bees/April2016Iteration/training 3/szymon_nytko/szymon_nytko.php
@@ -1,12 +1,27 @@
 <?php
 
-function reverse($y)
-{
+function sn_reverse($y){
 	$length = strlen($y);
 	for($i=$length-1;$i >=0;$i--){
 		echo $y[$i];
 	}
 }
-reverse("alaaaa");
-?>
+sn_reverse("alaaaa");
+
+function sn_palindrom($tekst){
+    $slowo = strlower($tekst);
+    $rozdziel = str_split($slowo);
+    $odwrocSlowo = "";
+    $dlugosc = strlen($slowo);
+    for($i = 0; $i < $dlugosc; $i++){
+    	$odwrocSlowo .= $rozdzielone[$dlugosc - $i - 1];
+    }
+        if( $slowo == $odwrocSlowo)
+            return "Palindrom! :)" ;
+        else
+            return "Nope, to nie jest palindrom :(";
+    }
+sn_palindrom("Ala");
+
+
 
