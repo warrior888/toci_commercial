66a3aff89e10cfe448873619c45903153b8a17f9
Edwin <wieszcz_pl@yahoo.com>
2016-04-06 19:54:59 +0200

    Poprawka if

--- bees/April2016Iteration/training 1/ej_tr1/ej_tr1.php
+++ bees/April2016Iteration/training 1/ej_tr1/ej_tr1.php
@@ -1,1 +1,50 @@
 <?php
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+	echo "\n\n";
+	echo 'Wybierz dzia³anie:';
+	echo "\n---------------------\n";
+	echo '1. Dodawanie'."\n";
+	echo '2. Odejmowanie'. "\n";
+	echo '3. Dzielenie'."\n";
+	echo '4. Mnozenie'. "\n\n";
+	
+	$rodzajDzialania = 6;
+	
+	$wartoscA = 15;
+	$wartoscB = 55;
+	
+	if($rodzajDzialania == 1)
+	{
+		echo 'Wybrales dodawanie'."\n";
+		$wynikDzialania = $wartoscA + $wartoscB;
+		echo $wartoscA .' + '. $wartoscB . ' = ' .$wartoscA+$wartoscB; // dlaczego jako wynik jest 70
+		echo "\n";
+		echo $wartoscA. ' + '. $wartoscB. ' = '. $wynikDzialania;
+	} elseif ($rodzajDzialania == 2)
+	{
+		echo 'Wybrales Odejmowanie'. "\n";
+		$wynikDzialania = $wartoscA - $wartoscB;
+		echo $wartoscA. ' - ' .$wartoscB . ' = ' .$wynikDzialania;
+		
+	} elseif ($rodzajDzialania == 3)
+	{
+		echo 'Wybrales Dzielenie'. "\n";
+		$wynikDzialania = $wartoscA/$wartoscB;
+		echo $wartoscA. ' / '. $wartoscB . ' = '.$wynikDzialania;
+		
+	} elseif ($rodzajDzialania == 4)
+	{
+		echo 'Wybrales Mnozenie'. "\n";
+		$wynikDzialania = $wartoscA * $wartoscB;
+		echo $wartoscA. ' * ' . $wartoscB . ' = ' .$wynikDzialania;
+		
+	} else
+	{
+		echo 'Nie ma takiego dzialania!';
+	}
+	echo "\n";
+	
+	echo "\n" . '---------------------------'."\n";
+
