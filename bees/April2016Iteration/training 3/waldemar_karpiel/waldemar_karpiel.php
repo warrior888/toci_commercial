9e8680154b9fab3b54345eb268bba345b752c27e
Waldemar <karpiel.waldemar@gmail.com>
2016-04-06 21:15:17 +0200

    strev + palindrome

--- bees/April2016Iteration/training 3/waldemar_karpiel/waldemar_karpiel.php
+++ bees/April2016Iteration/training 3/waldemar_karpiel/waldemar_karpiel.php
@@ -1,1 +1,31 @@
 <?php
+
+function wk_strrev($text)
+{
+    $odwrocony = '';
+
+    for ($i = (strlen($text) -1); $i >=0; $i--)
+    {
+        $odwrocony = $odwrocony . $text[$i];
+    }
+
+    return $odwrocony;
+}
+
+function wk_palindrome($text)
+{
+    return $text == wk_strrev($text);
+}
+
+//function wk_strpos($myString, $findMe)
+//{
+//    for ($i = 0; $i <= strlen($myString); $i++)
+//    {
+//        if ($myString == $findMe)
+//        {
+//
+//        }
+//    }
+//}
+
+echo wk_strpos('waldemar', 'ald');
