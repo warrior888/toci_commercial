e7a8e6d9f568c889f13915e543f691184ec67ef7
blazej doleska <blazej.doleska@gmail.com>
2016-04-06 20:50:30 +0200

    fix

--- bees/April2016Iteration/training 3/blazej_doleska/blazej_doleska.php
+++ bees/April2016Iteration/training 3/blazej_doleska/blazej_doleska.php
@@ -2,10 +2,14 @@
 
 function bd_strrev($str){
     $i = strlen($str);
+    $string = '';
     for($i; $i>=0; $i--){
         $string .= $str[$i];
     }
     echo $string;
+    echo "\n";
+
+
 }
 
 echo bd_strrev('blazej');
