779677a89400daaedd2576a0218fa944f262efc7
Edwin <wieszcz_pl@yahoo.com>
2016-04-06 18:54:33 +0200

    Homework - if

--- bees/April2016Iteration/training 1/ej_tr1/ej_tr1.php
+++ bees/April2016Iteration/training 1/ej_tr1/ej_tr1.php
@@ -1,57 +1,50 @@
 <?php
-
-	echo "\n";
-	echo "Hello world!";
-	echo "\n";
-	
-	//zmienna
-	$wiek = 25;
-	$nazwa = 'treœæ';
-	$limitWiekuDoroslosci = 18;
-	
-	//operatory
-	//+ - * / || && == === >< 
-	
-	//instrukcje warunkowe
-	if ($wiek >= $limitWiekuDoroslosci)
-	{
-		echo 'Osoba jest dorosla';
-		
-	} else {
-		
-		echo 'Mamy tu dziecko';
-	}
-	
 	echo "\n\n";
-	if($wiek > 6 && $wiek < 12)
+	echo 'Wybierz dzia³anie:';
+	echo "\n---------------------\n";
+	echo '1. Dodawanie'."\n";
+	echo '2. Odejmowanie'. "\n";
+	echo '3. Dzielenie'."\n";
+	echo '4. Mnozenie'. "\n\n";
+	
+	$rodzajDzialania = 6;
+	
+	$wartoscA = 15;
+	$wartoscB = 55;
+	
+	if($rodzajDzialania == 1)
 	{
-		echo 'Podstawówka';
-	}
-	
-	if($wiek >= 12 && $wiek < 15)
+		echo 'Wybrales dodawanie'."\n";
+		$wynikDzialania = $wartoscA + $wartoscB;
+		echo $wartoscA .' + '. $wartoscB . ' = ' .$wartoscA+$wartoscB; // dlaczego jako wynik jest 70
+		echo "\n";
+		echo $wartoscA. ' + '. $wartoscB. ' = '. $wynikDzialania;
+	} elseif ($rodzajDzialania == 2)
 	{
-		echo 'Gimbaza';
-	}
-	
-	if ($wiek >=15 && $wiek < 21)
+		echo 'Wybrales Odejmowanie'. "\n";
+		$wynikDzialania = $wartoscA - $wartoscB;
+		echo $wartoscA. ' - ' .$wartoscB . ' = ' .$wynikDzialania;
+		
+	} elseif ($rodzajDzialania == 3)
 	{
-		echo 'Szko³a œrednia';
-	}
-	
-	if ($wiek >=21 && $wiek < 25)
+		echo 'Wybrales Dzielenie'. "\n";
+		$wynikDzialania = $wartoscA/$wartoscB;
+		echo $wartoscA. ' / '. $wartoscB . ' = '.$wynikDzialania;
+		
+	} elseif ($rodzajDzialania == 4)
 	{
-		echo 'Studia';
-	}
-	
-	if($wiek >=25 && wiek < 29)
+		echo 'Wybrales Mnozenie'. "\n";
+		$wynikDzialania = $wartoscA * $wartoscB;
+		echo $wartoscA. ' * ' . $wartoscB . ' = ' .$wynikDzialania;
+		
+	} else
 	{
-		echo 'Praca';
+		echo 'Nie ma takiego dzialania!';
 	}
+	echo "\n";
 	
-	if($wiek >=29)
-	{
-		'Marna emerytura';
-	}
+	echo "\n" . '---------------------------'."\n";
 	
 	
-	//pêtle
+	
+	
