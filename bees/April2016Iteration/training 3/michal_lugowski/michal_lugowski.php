0a87ed9248ea810b189e6d670f23661beb285179
mihciu <michal_lugowski12@op.pl>
2016-04-07 18:34:34 +0200

    strrev, strpod, palidrom

--- bees/April2016Iteration/training 3/michal_lugowski/michal_lugowski.php
+++ bees/April2016Iteration/training 3/michal_lugowski/michal_lugowski.php
@@ -1,5 +1,34 @@
 <?php 
 
+//
+
+/*
+//strpos
+function ml_strpos($string, $find)
+{
+	$stringlen = strlen($string);
+	$found = '';
+	$numStr = '';
+	for ($i=0; $i<$stringlen; $i++)
+	{
+		if($string[$i]==$find[0])
+		{
+			$found=true;
+			$numStr=$i;
+			break;
+		}	
+	}
+
+	if($found==true)
+	{
+		echo 'Znaleziono '.$find.' w slowie: '.$string.' w '.$i.' miejscu';
+	} else {
+		echo 'Nie znaleziono '.$find.' w slowie: '.$string;
+	}
+}
+echo ml_strpos('lenovo', 'vo');
+
+//strrev
 function ml_strrev($slowo)
 {
 	$dlugosc=strlen($slowo);
@@ -14,4 +43,41 @@ function ml_strrev($slowo)
 
 echo ml_strrev('lenovo');
 
-?>
+
+//palidron
+$slowo = 'sedes';
+$spr = '';
+$dlSlowa=strlen($slowo);
+
+for($i=0; $i<=$dlSlowa-1; $i++)
+{
+	$spr.=$slowo[$i];
+}
+
+if($spr==$slowo){
+	echo 'palidron';
+} 
+else
+{
+	echo 'nie palidron';
+}
+
+
+$wiek = 19;
+
+if($wiek>=6 && $wiek<12)
+{
+	echo 'podstawowka';
+}
+else if($wiek >= 16 && $wiek < 19) 
+{
+	echo 'srednia';
+} 
+else if($wiek >= 19 && $wiek < 24) 
+{
+	echo 'studia';
+} 
+else 
+{
+	echo 'rodzina';
+*/
