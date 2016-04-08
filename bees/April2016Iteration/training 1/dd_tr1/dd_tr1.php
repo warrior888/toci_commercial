4a6f48e41f5ffd5962d5eb5a284f7c90a85ffbc3
dmnian <dmnian@gmail.com>
2016-04-08 19:41:06 +0200

    sprawdzanie wieku

--- bees/April2016Iteration/training 1/dd_tr1/dd_tr1.php
+++ bees/April2016Iteration/training 1/dd_tr1/dd_tr1.php
@@ -1,4 +1,40 @@
 <?php
 	echo "\n";
-	echo "To dziala";
+	echo 'To dziala';
 	echo "\n";
+	
+	//zmienne
+	$nazwa = 'tresc';
+	$wiek = 12;
+	$limitWiekuDoroslosci = 18;
+	
+	if($wiek >= $limitWiekuDoroslosci){
+		echo 'Osoba jest dorosla';
+	}
+	else {
+		echo 'Mamy tu dziecko';
+	}
+	
+	echo "\n";
+	
+	if($wiek > 6 && $wiek <12){
+		echo 'podstawowka';
+	}	 
+
+	if($wiek >= 12 && $wiek <16){
+		echo 'gimbaza';
+	}	 
+
+	if($wiek >= 16 && $wiek <19){
+		echo 'liceum';
+	}	 
+
+	if($wiek >= 19 && $wiek <26){
+		echo 'szkola srednia';
+	}	 
+	if($wiek >= 26){
+		echo 'praca zawodowa';
+	}	 
+	
+	echo "\n";
+	
