4710344c1ee11fe6987fdbdfab296c2cc2582eb0
bartdominiak <smugers@gmail.com>
2016-04-09 17:46:12 +0200

    strpos

--- bees/April2016Iteration/training 3/bartek_dominiak/bartek_dominiak.php
+++ bees/April2016Iteration/training 3/bartek_dominiak/bartek_dominiak.php
@@ -1,18 +1,15 @@
 <?php 
 
-//funkcje strlen strrev strpos substr palindrom strtr str_replace*
+//funkcje strpos palindrom strtr str_replace*
 
 //strlen
 	function bd_strlen ($string)	
-	{
-		$length = 0;
-		
+	{		
 		for ($i=0; isset($string[$i]); $i++) 
 		{
-			$length += 1;	
+			
 		}
-		
-		return $length;
+		return $i;
 	}
 	
 //strrev	
@@ -20,22 +17,47 @@
 	{
 		$stringRev = '';
 		
-		for ($i=bd_strlen($string)-1; $i>=0; $i--)
+		for ($i = bd_strlen($string) - 1; $i >= 0; $i--)
 		{
 			$stringRev .= $string[$i];
 		}
 		return $stringRev;
 	}
 	
-//strpos
-	function bd_strpos ($stringLonger, $stringMatch) 
+//substr
+	function bd_substr ($string, $needle, $length) 
 	{
+		$newString = '';
+		$newLength = $needle + $length;
 		
+		for ($i = $needle; $i <= $newLength && isset($string[$i]); $i++)
+		{
+			$newString .= $string[$i];
+		}		
+		return $newString;
 	}
 
+//strpos
+	function bd_strpos ($stringLonger, $stringNeedle) {
+		$position = null;
+		$stringNeedleLength = bd_strlen($stringNeedle);
+		for ($i = 0; $i <= bd_strlen($stringLonger); $i++)
+		{
+			if ($stringNeedle == bd_substr($stringLonger, $i, $stringNeedleLength)) 
+			{
+				return $position = $i;
+			}
+		}
+		return $position;
+	}
+	
+	
 $string = 'Przyklad';
-$stringLonger = 'Dluzszy ciag znakow do sprawdzenia';
-$stringMatch = 'do';
+$needle = 3;
+$length = 6;
+
+$stringLonger = "Lorem ipsum";
+$stringNeedle = "ipsum";
 
 //wypisywanie
 echo "\n";
@@ -49,7 +71,12 @@ echo "\n";
 echo 'Robie lustrzane odbicie wyrazu "' . $string . '": ' . bd_strrev($string);
 
 echo "\n";
+echo '** Funkcja substr **';
+echo "\n";
+echo 'Wyraz: "' . $string . '" Od:' . $needle .' o dlugosci:' . $length . ' Wynik: ' . bd_substr($string, $needle, $length);
+
+echo "\n";
 echo '** Funkcja strpos **';
 echo "\n";
-echo 'Szukam wyrazu "'. $stringMatch .'" w ciagu znakow: "' . $stringLonger . '": ' . bd_strpos($stringLonger, $stringMatch);
+echo 'Wyraz:' . bd_strpos($stringLonger, $stringNeedle);
 
