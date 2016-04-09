d76650a0c25a03a57c4f758be7877d355d10a728
dmnian <dmnian@gmail.com>
2016-04-09 19:07:36 +0200

    Dodałem str_replace i poprawiłem strpos.

--- bees/April2016Iteration/training 3/damian_dabrowski/damian_dabrowski.php
+++ bees/April2016Iteration/training 3/damian_dabrowski/damian_dabrowski.php
@@ -27,8 +27,44 @@ function dd_substr($od, $przez, $wyraz){
 	$subs = "";
 	for($i = $od; $i < $od + $przez; $i++ ){
 		$subs .= $wyraz[$i];
-	}	
+	}
 	return $subs;
+}
+
+function dd_strpos($fragment ,$wyraz){
+	$dlugoscFrag = dd_strlen($fragment);
+	$dlugoscWyr = dd_strlen($wyraz);
+
+	for ($i = 0; $i <= ($dlugoscWyr - $dlugoscFrag); $i++) {
+		if( dd_substr($i, $dlugoscFrag, $wyraz) == $fragment) return $i;
+	}
+	return "nie ma takiego podciągu!";
+}
+
+function dd_str_replace($co,$naCo, $wyraz){
+	$wyrazZmieniony = "";
+	$pozycja = dd_strpos($co, $wyraz);
+
+	if($pozycja === "nie ma takiego podciągu!") {
+		return $pozycja;
+	} else {
+
+		$dlugosc = dd_strlen($co);
+
+		$czyWklejony = false;
+		for($i = 0; $i <dd_strlen($wyraz); $i++){
+			if($i < $pozycja || ($i > ($pozycja + $dlugosc-1))) {
+				$wyrazZmieniony .= $wyraz[$i];
+			}
+			else {
+				if(!$czyWklejony){
+					$wyrazZmieniony .= $naCo;
+				}
+				$czyWklejony = true;
+			}
+		}
+		return $wyrazZmieniony;
+	}
 }
 
 echo dd_strrev('text');
@@ -39,21 +75,10 @@ if(dd_isPalindrom("ala")){
 	echo 'Wyraz nie jest palindromem!';
 }
 
-function dd_strpos($fragment ,$wyraz){
-	$dlugoscFrag = dd_strlen($fragment);
-	$dlugoscWyr = dd_strlen($wyraz);
-	
-	for ($i = 0; $i < ($dlugoscWyr - $dlugoscFrag); $i++) {
-		if( dd_substr($i, $dlugoscFrag, $wyraz) === $fragment) return $i;
-	}
-	return "nie ma!";
-}
-
 $napis = 'neandertalczyk';
 echo "\n";
 echo dd_substr(2, 4, $napis);
 echo "\n";
 echo 'fragment znajduje sie na pozycji: '.dd_strpos('tal', $napis);
-
-
+echo "\nZamiana substr: ".dd_str_replace('Hello', 'Damian', 'Hello world HAHA');
 ?>
