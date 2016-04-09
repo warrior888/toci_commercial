45a3c4c159ecf7d66cf1774ec9c82904cfabb06f
Shirobachi <szevaa97@gmail.com>
2016-04-09 17:41:33 +0200

    training 1 .. pierwsze ify petle i zmienn ($/$$/$$$/ ... )

--- bees/April2016Iteration/training 4/Shirobachi/index.php
+++ bees/April2016Iteration/training 4/Shirobachi/index.php
@@ -1,1 +1,67 @@
 <?php
+
+	/*$rok = 2016;
+	$rok = "jakis text";
+	
+	echo $rok;
+	
+	$rok = 5;
+	
+	echo "\n\n" . $rok . "\n\n\n";
+	
+	$rok = 2015;
+	
+	if(123==123131) 
+	{
+		$rok = 2016;
+	}
+	
+	$licznikGorny = 0;
+	$licznikSrodkowy = 0;
+	$licznikDolny = 0;
+	
+	$tekst = 0;
+		
+		for($licznikGorny = 0; $licznikGorny < 8; $licznikGorny++)
+			{
+				echo 'text' . "\n";
+				$tekst++;
+				//$licznikGorny--;
+				
+				//if($licznikGorny == 3)
+				//{
+				//	break;
+				//}
+				
+			}
+	
+	echo $tekst;*/
+	
+	$ciekawa = 'piekna';
+	$piekna = 'sliczna';
+	$sliczna = 'ciekawa';
+	
+	//echo $$$$$$$$$$$$$$$$$$$$$$$ciekawa;
+	
+	//pierwsza();
+	$fn = 'pierwsza';
+	
+	$fn();
+	
+	function Pierwsza()
+	{
+		echo 'pierwsza dziala';		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
