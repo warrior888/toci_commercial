07b4e16d9bb14f501e2ea614df6d1feaafb393a7
bartdominiak <smugers@gmail.com>
2016-04-09 11:44:27 +0200

    Problem z zadaniem

--- bees/April2016Iteration/training 3/bartek_dominiak/bartek_dominiak.php
+++ bees/April2016Iteration/training 3/bartek_dominiak/bartek_dominiak.php
@@ -1,10 +1,14 @@
-<?php
+<?php 
 
-	function length($string)
+//funkcje strlen strpos strrev substr palindrom strtr str_replace*
+
+//strlen
+
+	function bd_strlen ($string)	
 	{
 		$length = 0;
 		
-		for($i = 0; isset($string[$i]); $i++ )
+		for ($i=0; isset($string[$i]); $i++) 
 		{
 			$length += 1;	
 		}
@@ -12,6 +16,27 @@
 		return $length;
 	}
 	
-	//tablice
+//strrev	
+	function bd_strrev ($string) 
+	{
+		$stringRev = '';
+		
+		for ($i=bd_strlen($string); $i>=0; $i--)
+		{
+			$stringRev .= $string[$i];
+		}
+		return $stringRev;
+	}
+
+$string = 'Przyklad';	
 	
-	//funkcje strpos strrev palindrom strtr str_replace
+//wypisywanie
+echo "\n";
+echo '** Funkcja strlen **';
+echo "\n";
+echo 'Licze ilosc liter w wyrazie "' . $string . '": ' . bd_strlen($string);
+
+echo "\n";
+echo '** Funkcja strrev **';
+echo "\n";
+echo 'Robie lustrzane odbicie wyrazu "' . $string . '": ' . bd_strrev($string);
