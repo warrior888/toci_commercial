3ddd0f5d1679ad39dc51dff39621b688cbad1168
Shirobachi <szevaa97@gmail.com>
2016-04-12 18:29:22 +0200

    PYTANIE! Zadanie z przepisaniem liczby slowami. Pytanie linia 65

--- bees/April2016Iteration/training 4/Shirobachi/gimnastyka.php
+++ bees/April2016Iteration/training 4/Shirobachi/gimnastyka.php
@@ -26,3 +26,48 @@
 	{
 		echo 'Piaty tydzien';
 	}
+	
+	
+	for($i=0; $i < $wartosc; $i++)
+	{
+		if($i == 1)
+		{
+			echo 'jeden ';
+		}
+		else if($i == 2)
+		{
+			echo 'dwa ';
+		}
+		else if($i == 3)
+		{
+			echo 'trzy ';
+		}
+		else if($i == 4)
+		{
+			echo 'cztery ';
+		}
+		else if($i == 5)
+		{
+			echo 'piec ';
+		}
+		else if($i == 6)
+		{
+			echo 'szesc ';
+		}
+		else if($i == 7)
+		{
+			echo 'siedem ';
+		}
+		else if($i == 8)
+		{
+			echo 'osiem ';
+		}
+		else if($i == 9)
+		{
+			echo 'dziewiec ';
+		}
+		else if($i == 0)
+		{
+			echo 'zero ';
+		}
+	}
