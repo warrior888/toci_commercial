a9e9430b149ed27babefb2de70339b122990c541
Shirobachi <szevaa97@gmail.com>
2016-04-18 14:14:15 +0200

    no message

--- bees/April2016Iteration/training 4/Shirobachi/hashowanienrkonta.php
+++ bees/April2016Iteration/training 4/Shirobachi/hashowanienrkonta.php
@@ -2,11 +2,12 @@
 
 function hashon($nr)
 {
-	$litery = array('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m');//25
+	$litery = array('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m');//25 indexow 
 	for($i=0; $i<rand(25, 250); $i++)
 	{
-		$nr[rand(0, strlen($nr-1))] .= $litery[rand(0, 25)];		
-	}
+		//$nr[rand(0, strlen($nr-1))] .= $litery[rand(0, 25)];		
+		$nr = substr_replace($nr, $litery[rand(0, 25)], rand(0, strlen($nr)-1), 0);// to się nie wykonuje, ale dlaczego ?
+	}//$nr[rand(0, strlen($nr)-1)]
 	
 	return $nr;
 }
@@ -16,10 +17,9 @@ function hashoff()
 	return 0;
 }
 
-$text = '1516534134683163';
+$text = '76 1160 2202 0000 0002 9799 1972';
 
 echo $text . "\n";
 hashon($text);
 
-echo $text . "\n";
 echo $text . "\n";
