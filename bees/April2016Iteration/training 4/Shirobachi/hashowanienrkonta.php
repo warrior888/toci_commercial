2c2eabdea03e0285aa2b2623fa2323097a980fdd
Shirobachi <szevaa97@gmail.com>
2016-04-19 07:46:02 +0200

    no message

--- bees/April2016Iteration/training 4/Shirobachi/hashowanienrkonta.php
+++ bees/April2016Iteration/training 4/Shirobachi/hashowanienrkonta.php
@@ -3,7 +3,7 @@
 function hashon($nr)
 {
 	$litery = array('q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m');//25 indexow 
-	for($i=0; $i<rand(25, 250); $i++)
+	for($i=0; $i<rand(1000, 10000); $i++)
 	{
 		//$nr[rand(0, strlen($nr-1))] .= $litery[rand(0, 25)];		
 		$nr = substr_replace($nr, $litery[rand(0, 25)], rand(0, strlen($nr)-1), 0);// to się nie wykonuje, ale dlaczego ?
@@ -12,14 +12,48 @@ function hashon($nr)
 	return $nr;
 }
 
-function hashoff()
+function hashoff($word)
 {
-	return 0;
+	$number = '';
+	
+	for($i=0; $i<strlen($word); $i++)
+	{
+		switch($word[$i])
+		{
+			case"0":
+				$number .=0;break;
+			case'1':
+				$number .=1;break;
+			case'2':
+				$number .=2;break;
+			case'3':
+				$number .=3;break;
+			case'4':
+				$number .=4;break;
+			case'5':
+				$number .=5;break;
+			case'6':
+				$number .=6;break;
+			case'7':
+				$number .=7;break;
+			case'8':
+				$number .=8;break;
+			case'9':
+				$number .=9;break;
+			case' ':
+				$number .=' ';break;
+		}//switch
+	}//for
+	
+	return $number;
 }
 
 $text = '76 1160 2202 0000 0002 9799 1972';
 
 echo $text . "\n";
-hashon($text);
 
+$text = hashon($text);
+echo $text . "\n";
+
+$text = hashoff($text);
 echo $text . "\n";
